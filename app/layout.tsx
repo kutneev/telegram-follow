f7ac2003cd3c6e2005fbe52aa35e82f92b9263b0
Ruslan Kutneev <ruslan.kutneev@gmail.com>
2026-06-01 09:43:07 +0300

    Install Yandex tracking

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from "next";
+import Script from "next/script";
 import type { ReactNode } from "react";
 import "./globals.css";
 
@@ -25,6 +26,9 @@ export const metadata: Metadata = {
     index: true,
     follow: true
   },
+  verification: {
+    yandex: "5f558a8f36aaed6b"
+  },
   icons: {
     icon: "/icon.svg"
   }
@@ -37,7 +41,40 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="ru">
-      <body>{children}</body>
+      <body>
+        {children}
+        <Script id="yandex-metrika" strategy="afterInteractive">
+          {`
+            (function(m,e,t,r,i,k,a){
+              m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+              m[i].l=1*new Date();
+              for (var j = 0; j < document.scripts.length; j++) {
+                if (document.scripts[j].src === r) { return; }
+              }
+              k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)
+            })(window, document, 'script', 'https://mc.yandex.ru/metrika/tag.js?id=109558727', 'ym');
+
+            ym(109558727, 'init', {
+              ssr: true,
+              webvisor: true,
+              clickmap: true,
+              ecommerce: 'dataLayer',
+              accurateTrackBounce: true,
+              trackLinks: true
+            });
+          `}
+        </Script>
+        <noscript>
+          <div>
+            {/* eslint-disable-next-line @next/next/no-img-element */}
+            <img
+              src="https://mc.yandex.ru/watch/109558727"
+              style={{ position: "absolute", left: "-9999px" }}
+              alt=""
+            />
+          </div>
+        </noscript>
+      </body>
     </html>
   );
 }
